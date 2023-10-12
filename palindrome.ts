7087595bc688a55e497511f8cf955e62eee1f5b1
Yalalt <yalaltgandush@gmail.com>
2023-10-12 23:56:35 +0800

    To check function Palindrom number

---
+++ palindrome.ts
@@ -0,0 +1,13 @@
+function isPalindrome(x: number): boolean {
+    let result = x;
+    let modToo;
+    let sum = 0;
+
+    while(x >= 1) {
+        modToo = x % 10;
+        sum = (sum * 10) + modToo;
+        x = Math.floor(x / 10);
+    }
+
+    return result === sum;
+};
